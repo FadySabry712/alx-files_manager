1941c3c9f41c8d5f7ffa9ec6a5be60fce0da9cf7
FadySabry712 <fadysabry95@yahoo.com>
2025-06-27 19:38:22 -0700

    eslint for server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
+
 import express from 'express';
 import router from './routes/index';
 
-
 const port = parseInt(process.env.PORT, 10) || 5000;
 
-
 const app = express();
-
 
 app.use(express.json());
 app.use('/', router);
@@ -14,6 +12,5 @@ app.use('/', router);
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
-
 
 export default app;
